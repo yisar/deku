a97b5b05c644581b95288358af4bec9d4d8f5e84
132yse <1533540012@qq.com>
2020-05-02 16:02:35 +0800

    websocket

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { h, render, useState } from './fre.js'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{count}{555}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   )
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { Application, NotFoundException } from 'https://deno.land/x/abc@v0.2.11/mod.ts'
 import { logger } from 'https://deno.land/x/abc@v0.2.11/middleware/logger.ts'
 import { join } from 'https://deno.land/std@v0.42.0/path/mod.ts'
-const { readFile, transpileOnly } = Deno
+import { serve } from 'https://deno.land/std@v0.42.0/http/server.ts'
+import { acceptWebSocket, WebSocket } from 'https://deno.land/std@v0.42.0/ws/mod.ts'
+const { readFile, transpileOnly, watchFs, cwd } = Deno
 
 const app = new Application()
 app.start({ port: 8887, hostname: '0.0.0.0' })
 console.log('Server running at http://127.0.0.1:8887/')
 
-app.use(logger()).get('/*files', async c => {
+app.use(logger()).get('/*files', async (c) => {
   if (c.path === '/') {
     return c.redirect('/index.html')
   }
@@ -21,6 +23,36 @@ app.use(logger()).get('/*files', async c => {
 
   throw new NotFoundException()
 })
+
+//ws server
+const port = '8080'
+console.log(`111`)
+for await (const req of serve(`:${port}`)) {
+  const { headers, conn } = req
+  acceptWebSocket({
+    conn,
+    headers,
+    bufReader: req.r,
+    bufWriter: req.w,
+  }).then(
+    async (sock: WebSocket): Promise<void> => {
+      const it = sock.receive()
+      const iter = watchFs('/')
+      for await (const event of iter) {
+        console.log(event)
+        sock.send(event.paths[0])
+      }
+
+      while (true) {
+        const { done, value } = await it.next()
+        if (done) break
+        if (typeof value === 'string') {
+          await sock.send(value)
+        }
+      }
+    }
+  )
+}
 
 async function transform(rootName: string, source: string) {
   const result = await transpileOnly(
