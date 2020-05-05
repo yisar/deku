fcca3f1804234e04c9f85c335015ab77a77b2625
132yse <1533540012@qq.com>
2020-05-05 10:41:30 +0800

    beatify template

--- template/app.js
+++ template/app.js
@@ -3,9 +3,9 @@ import { h, useState } from './web_modules/fre.js'
 export default function App() {
   const [count, setCount] = useState(0)
   return (
-    <div>
+    <div class='container'>
       <h1>hello fre!</h1>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => setCount(count + 1)}>click: {count}</button>
     </div>
   )
 }
